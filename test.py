125a4ec29f6772ac423e15221c3a9291bad8585e
SiraDD <44273602+SiraDD@users.noreply.github.com>
2019-11-27 14:07:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 def fibo(number):
-  number1=1
-  number2=1
-  for i in range(number):
-    print(number2)
-    temp=number2
-    number2 += number1
-    number1 = temp
+    number1=0
+    number2=1
+    for i in range(number):
+        temp=number2
+        number2 += number1
+        number1 = temp
+        print(number2/number1)
